9129b0253af17e249401dd7ed87f268cef25ee17
user <76937231+Tus1688@users.noreply.github.com>
2022-08-20 14:27:02 +0700

    feat(engine): use selenium

--- engine.py
+++ engine.py
@@ -2,30 +2,69 @@ import random
 import sys
 
 import requests
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 from ui import print_error, print_info
 
-cookie = "" # cookie from first request / provided by user
+initial_cookie = [{}] # cookie from first request / provided by user
 
 def engine(url, depth, threads, output, user_agent, proxy, cookie):
-    print("\nfrom engine.py \n")
-    print(url, depth, threads, output, user_agent)
-    print(getUserAgent(False))
-    
     try:
         r = requests.get(url)
         print_info('Url is valid')
-        curl(url)
+        req_headless_browser(url)
     except Exception as e:
         print_error(str(e))
         sys.exit(1)
 
-def curl(url):
-    # make request with custom user agent
-    headers = getUserAgent()
+def req_headless_browser(url, cookie = None):
+    print_info(f'Requesting {url} using headless browser')
+    options = Options()
+    options.headless = True
+
+    options.add_argument('--disable-cache')
+    options.add_argument('--disable-telemetry')
+    # options resist fingerprinting
+    options.add_argument('--lang=en-US')
+    options.add_argument('--disable-client-side-phishing-detection')
+
+    driver = webdriver.Firefox(executable_path='geckodriver',options=options)
+    driver.delete_all_cookies()
+
+    # use custom user agent from getUserAgent()
+    driver.execute_script("window.navigator.userAgent = '{}'".format(getUserAgent(True)))
+
+    # prevent browser to be fingerprinted
+    driver.execute_script("window.navigator.webdriver = false")
+    driver.execute_script("window.navigator.languages = ['en-US', 'en']")
+    driver.execute_script("window.navigator.platform = '{}'".format(random.choice(['MacIntel', 'Win32', 'Linux x86_64'])))
+    driver.execute_script("window.navigator.product = '{}'".format(random.choice(['Gecko', 'Gecko/20100101 Firefox/{}'.format(random.randint(1, 60))])))
+    driver.execute_script("window.navigator.productSub = '{}'".format(random.choice(['20030107', '20100101', '20100101', '20100101'])))
+    driver.execute_script("window.navigator.vendor = '{}'".format(random.choice(['Mozilla', 'Mozilla/5.0'])))
+    driver.execute_script("window.navigator.vendorSub = '{}'".format(random.choice(['20100101', '20100101', '20100101', '20100101'])))
+    driver.execute_script("window.navigator.buildID = '{}'".format(random.choice(['20100101', '20100101', '20100101', '20100101'])))
+
+    driver.get(url)
+
+    html_result = driver.page_source
+    #print(html_result)
+    initial_cookie = driver.get_cookies()
+
+    driver.close()
+    # print request and response
+
+    for cookie in initial_cookie:
+        print(cookie)
+
+def req_python_request(url, cookie):
+    print_info(f'Requesting {url} using python requests')
+    headers = {
+        'User-Agent': getUserAgent(True),
+        'Cookie': cookie
+    }
     r = requests.get(url, headers=headers)
-    # make another request with r cookies
-    r2 = requests.get(url, headers=headers, cookies=r.cookies)
+    print(r.text)
 
 def getUserAgent(value:bool = False) -> str:
     if value:
