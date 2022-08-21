a3ba9e3ac89c859d52f2551663a7fca0a289db53
user <76937231+Tus1688@users.noreply.github.com>
2022-08-21 13:18:53 +0700

    feat(browser): enhance profiles

--- browser.py
+++ browser.py
@@ -7,8 +7,13 @@ def custom_profile(proxy:str = None, user_agent:str = None) -> webdriver.Firefox
     
     profile.set_preference('dom.webdriver.enabled', False)
     profile.set_preference('useAutomationExtension', False)
-    # disable telemetry
-    profile.set_preference('toolkit.telemetry.enabled', False)
+
+    # startup page
+    profile.set_preference('browser.startup.page', 0)
+    profile.set_preference('browser.startup.homepage', 'about:blank')
+    profile.set_preference('browser.newtab.preload', False)
+    
+    # newtab page
     profile.set_preference('browser.newtabpage.activity-stream.feeds.telemetry', False)
     profile.set_preference('browser.newtabpage.activity-stream.telemetry', False)
     profile.set_preference('browser.newtabpage.activity-stream.feeds.snippets', False)
@@ -17,6 +22,26 @@ def custom_profile(proxy:str = None, user_agent:str = None) -> webdriver.Firefox
     profile.set_preference('browser.newtabpage.activity-stream.showSponsored', False)
     profile.set_preference('browser.newtabpage.activity-stream.feeds.discoverystreamfeed', False)
     profile.set_preference('browser.newtabpage.activity-stream.showSponsoredTopSites', False)
+    profile.set_preference('browser.newtabpage.activity-stream.default.sites', "")
+
+    # disable geolocation
+    profile.set_preference('geo.provider.ms-windows-location', False)
+    profile.set_preference('geo.provider.use_corelocation', False)
+    profile.set_preference('geo.provider.use_gpsd', False)
+    profile.set_preference('geo.provider.use_geoclue', False)
+
+    # disable region updates
+    profile.set_preference('browser.region.network.url', '')
+    profile.set_preference('browser.region.update.enabled', False)
+    profile.set_preference('intl.accept_languages', 'en-US, en')
+    profile.set_preference('javascript.use_us_english_locale', True)
+
+    # addons 
+    profile.set_preference('extensions.getAddons.showPane', False)
+    profile.set_preference('extensions.htmlaboutaddons.recommendations.enabled', False)
+    profile.set_preference('browser.discovery.enabled', False)
+
+    # telemetry
     profile.set_preference('datareporting.policy.dataSubmissionEnabled', False)
     profile.set_preference('datareporting.healthreport.uploadEnabled', False)
     profile.set_preference('toolkit.telemetry.unified', False)
@@ -41,7 +66,18 @@ def custom_profile(proxy:str = None, user_agent:str = None) -> webdriver.Firefox
     profile.set_preference('captivedetect.canonicalURL', '')
     profile.set_preference('network.captive-portal-service.enabled', False)
     profile.set_preference('network.connectivity-service.enabled', False)
-    profile.set_preference('browser.safebrowsing.downloads.remote.enabled', False)
+
+    # block implicit outbound
+    profile.set_preference('network.prefetch-next', False)
+    profile.set_preference('network.dns.disablePrefetch', True)
+    profile.set_preference('network.predictor.enabled', False)
+    profile.set_preference('network.predictor.enable-prefetch', False)
+    profile.set_preference('network.http.speculative-parallel-limit', 0)
+    profile.set_preference('browser.places.speculativeConnect.enabled', False)
+    profile.set_preference('network.dns.disableIPv6', True)
+
+    profile.set_preference('accessibility.force_disabled', 1)
+    profile.set_preference('beacon.enabled', False)
 
     # disable cache
     profile.set_preference('browser.cache.disk.enable', False)
@@ -54,6 +90,15 @@ def custom_profile(proxy:str = None, user_agent:str = None) -> webdriver.Firefox
     profile.set_preference('privacy.resistFingerprinting', True)
     profile.set_preference('privacy.resistFingerprinting.block_mozAddonManager', True)
     profile.set_preference('privacy.resistFingerprinting.letterboxing', True)
+    profile.set_preference('browser.startup.page', False)
+    profile.set_preference('browser.display.use_system_colors', False)
+    profile.set_preference('widget.non-native-theme.enabled', True)
+    profile.set_preference('extensions.webcompat-reporter.enabled', False)
+
+    profile.set_preference('browser.toolbars.bookmarks.visibility', "never")
+    profile.set_preference('browser.newtabpage.activity-stream.feeds.favicon', False)
+    profile.set_preference('browser.tabs.loadBookmarksInBackground', False)
+    profile.set_preference('browser.newtabpage.activity-stream.section.highlights.includeBookmarks', False)
 
     # check if proxy is set
     if proxy:
